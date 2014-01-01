37bc1ee0c71fa12d9364d5d7d226993fdaf987e7
Richard North <rich.north@gmail.com>
2014-01-01 23:01:26 +0000

    Adding Javadocs

--- src/main/java/com/deloittedigital/testpackage/junitcore/FailFastSupportCore.java
+++ src/main/java/com/deloittedigital/testpackage/junitcore/FailFastSupportCore.java
@@ -7,13 +7,25 @@ import org.junit.runner.notification.RunListener;
 import org.junit.runner.notification.RunNotifier;
 
 /**
+ * Replacement for JUnitCore which provides access to its RunNotifier.
+ *
  * Created by richardnorth on 01/01/2014.
  */
 public class FailFastSupportCore {
 
     private final RunNotifier notifier = new RunNotifier();
 
-    public Result run(Runner runner) {
+    /**
+     * Run all the tests contained in <code>request</code>.
+     *
+     * @param request the request describing tests
+     * @return a {@link Result} describing the details of the test run and the failed tests.
+     */
+    public Result run(Request request) {
+        return run(request.getRunner());
+    }
+
+    private Result run(Runner runner) {
         Result result = new Result();
         RunListener listener = result.createListener();
         notifier.addFirstListener(listener);
@@ -27,19 +39,31 @@ public class FailFastSupportCore {
         return result;
     }
 
+    /**
+     * Add a listener to be notified as the tests run.
+     *
+     * @param listener the listener to add
+     * @see org.junit.runner.notification.RunListener
+     */
     public void addListener(RunListener listener) {
         notifier.addListener(listener);
     }
 
+    /**
+     * Remove a listener.
+     *
+     * @param listener the listener to remove
+     */
     public void removeListener(RunListener listener) {
         notifier.removeListener(listener);
     }
 
+    /**
+     * Get the Run Notifier.
+     *
+     * @return the JUnit run notifier
+     */
     public RunNotifier getNotifier() {
         return notifier;
-    }
-
-    public Result run(Request request) {
-        return run(request.getRunner());
     }
 }
